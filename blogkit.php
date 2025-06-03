14c0a55f68adf7a7552ea9b1d07fc2811cba5a8c
dreamableai <dreamableai@gmail.com>
2025-06-03 18:01:07 +0600

    conditional check

--- blogkit.php
+++ blogkit.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: BlogKit - Advanced Blog Elements for Elementor
  * Description: A powerful toolkit for enhancing your WordPress blog with custom features and performance improvements.
- * Version: 0.1.0
+ * Version: 1.0.0
  * Author: SupreoX Limited
  * Author URI: https://supreox.com
  * License: GPLv2 or later
@@ -12,7 +12,7 @@
  * Requires at least: 5.8
  * Requires PHP: 8.0
  * namespace: blogkit
- * Requires Plugins: elementor
+
  * 
  * @package BlogKit
  */
@@ -58,12 +58,12 @@ final class BlogKit
      */
     private function define_constants()
     {
-        define('BLOGKIT_VERSION', '0.1.0');
+        define('BLOGKIT_VERSION', '1.0.0');
         define('BLOGKIT_PATH', plugin_dir_path(__FILE__));
         define('BLOGKIT_URL', plugin_dir_url(__FILE__));
         define('BLOGKIT_BASENAME', plugin_basename(__FILE__));
         define('BLOGKIT_FILE', __FILE__);
-        define('BLOGKIT_NAME', 'blogkit');
+        define('BLOGKIT_NAME', 'BlogKit');
     }
 
     /**
@@ -81,7 +81,7 @@ final class BlogKit
      */
     private function init_hooks()
     {
-        add_action('plugins_loaded', [$this, 'plugin_loaded']);       
+        add_action('plugins_loaded', [$this, 'plugin_loaded']);
         register_activation_hook(BLOGKIT_FILE, [$this, 'activate']);
         register_deactivation_hook(BLOGKIT_FILE, [$this, 'deactivate']);
     }
@@ -91,8 +91,19 @@ final class BlogKit
      */
     public function plugin_loaded()
     {
-        if (class_exists('BlogKit\Manager')) {
-            new \BlogKit\Manager();
+        // Check if Elementor is active and loaded
+        if (did_action('elementor/loaded') && class_exists('\Elementor\Plugin')) {
+            if (class_exists('BlogKit\Manager')) {
+                new \BlogKit\Manager();
+            }
+        } else {
+            // Show admin notice if Elementor is not installed or activated
+            add_action('admin_notices', function () {
+                $elementor_url = admin_url('plugin-install.php?s=elementor&tab=search&type=term');
+                echo '<div class="notice notice-error is-dismissible">';
+                echo '<p><strong>' . BLOGKIT_NAME . '</strong> requires <a href="' . esc_url($elementor_url) . '" target="_blank">Elementor</a> to be installed and activated.</p>';
+                echo '</div>';
+            });
         }
     }
 
