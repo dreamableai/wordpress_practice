be67a9be121d77f4d0db34025a1feb44c1194807
dreamableai <dreamableai@gmail.com>
2025-06-15 13:01:07 +0600

    categoires markup

--- Frontend/Elementor/Widgets/TaxonomyList/RenderView.php
+++ Frontend/Elementor/Widgets/TaxonomyList/RenderView.php
@@ -1,2 +1,37 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if (!defined('ABSPATH'))
+    exit; // Exit if accessed directly
+
+?>
+<!-- Taxonomy List Area-->
+<div class="blogkit-taxonomy-list-area">
+    <!-- Taxonomy Heading -->
+    <div class="blogkit-taxonomy-heading">
+        <h3>Categories</h3>
+    </div><!-- Taxonomy Heading -->
+    <!-- Taxonomy List -->
+    <div class="blogkit-taxonomy-list">
+        <!-- Single Taxonomy -->
+        <div class="blogkit-single-taxonomy">
+            <a href="#">
+                <span class="blogkit-taxonomy-name">Technology</span>
+                <span class="blogkit-taxonomy-count">22</span>
+            </a>
+        </div><!-- Single Taxonomy -->
+        <!-- Single Taxonomy -->
+        <div class="blogkit-single-taxonomy">
+            <a href="#">
+                <span class="blogkit-taxonomy-name">Business</span>
+                <span class="blogkit-taxonomy-count">22</span>
+            </a>
+        </div><!-- Single Taxonomy -->
+        <!-- Single Taxonomy -->
+        <div class="blogkit-single-taxonomy">
+            <a href="#">
+                <span class="blogkit-taxonomy-name">Politics</span>
+                <span class="blogkit-taxonomy-count">22</span>
+            </a>
+        </div><!-- Single Taxonomy -->
+
+    </div><!-- Taxonomy List -->
+</div><!-- Taxonomy List Area-->
